9a142679fb07110578fc06216069bc7f92c0cdfa
gabriel <metalmadnes98@gmail.com>
2022-05-06 17:07:02 -0500

    Modiicacion para trabajar con la API deseada

--- JS/consultaAPI1.js
+++ JS/consultaAPI1.js
@@ -1,21 +1,19 @@
-const fetchPokemon = () => {
-    const pokeNameInput = document.getElementById("pokeName");
-    let pokeName = pokeNameInput.value;
-    pokeName = pokeName.toLowerCase();
-    const url = `https://pokeapi.co/api/v2/pokemon/${pokeName}`;
+const fetchFood = () => {
+    // const pokeNameInput = document.getElementById("pokeName");
+    // let pokeName = pokeNameInput.value;
+    // pokeName = pokeName.toLowerCase();
+    const url = `www.themealdb.com/api/json/v1/1/random.php`;
     fetch(url).then((res) => {
         if (res.status != "200") {
+            console.log("aaa")
             console.log(res);
-            pokeImage("./pokemon-sad.gif")
         } else {
             return res.json();
         }
     }).then((data) => {
         if (data) {
+            console.log("cccc")
             console.log(data);
-            let pokeImg = data.sprites.front_default;
-            pokeImage(pokeImg);
-            console.log(pokeImg);
         }
     });
 }

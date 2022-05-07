44aac578d5e838b059f4fc11c9bf8ceeaecda76c
gabriel <metalmadnes98@gmail.com>
2022-05-07 03:15:44 -0500

    Se agregaron los elementos en pantalla correctamente, ademas de que se muestra la imagen del platillo en pantalla

--- JS/consultaAPI1.js
+++ JS/consultaAPI1.js
@@ -54,48 +54,12 @@ mealTags.textContent = meal.strTags;
 // youtube o video del platillo
 console.log(meal.strYoutube)
 mealVideo.textContent = meal.strYoutube;
-
-// const newInnerHTML = `
-// <div class="row">
-// <div class="columns five">
-//     <img src="${meal.strMealThumb}" alt="Meal Image">
-//     ${
-//         meal.strCategory
-//             ? `<p><strong>Category:</strong> ${meal.strCategory}</p>`
-//             : ''
-//     }
-//     ${meal.strArea ? `<p><strong>Area:</strong> ${meal.strArea}</p>` : ''}
-//     ${
-//         meal.strTags
-//             ? `<p><strong>Tags:</strong> ${meal.strTags
-//                     .split(',')
-//                     .join(', ')}</p>`
-//             : ''
-//     }
-//     <h5>Ingredients:</h5>
-//     <ul>
-//         ${ingredients.map(ingredient => `<li>${ingredient}</li>`).join('')}
-//     </ul>
-// </div>
-// <div class="columns seven">
-//     <h4>${meal.strMeal}</h4>
-//     <p>${meal.strInstructions}</p>
-// </div>
-// </div>
-// ${
-// meal.strYoutube
-//     ? `
-// <div class="row">
-// <h5>Video Recipe</h5>
-// <div class="videoWrapper">
-//     <iframe width="420" height="315"
-//     src="https://www.youtube.com/embed/${meal.strYoutube.slice(-11)}">
-//     </iframe>
-// </div>
-// </div>`
-//     : ''
-// }
-// `;
-
-// meal_container.innerHTML = newInnerHTML;
+// cambiar imagen
+let mealImage = meal.strMealThumb;
+mealImg(mealImage);
 };
+const mealImg = (url) => {
+    const mealImg = document.getElementById("mealImg")
+    mealImg.src = url;
+}
+// https://www.florin-pop.com/blog/2019/09/random-meal-generator/
